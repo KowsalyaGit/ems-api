0653c3f247bf3f20a427806021aa0d874780df4f
Kavin S <kavingct@gmail.com>
2021-05-02 10:38:28 +0530

    Payment change and .env file

--- controller/paymentController.js
+++ controller/paymentController.js
@@ -3,9 +3,9 @@ const crypto = require('crypto');
 
 exports.getUrl = (req, res) => {
 
-    let msg = process.env.BILLDESK_MERCHANT_ID+ "|"+ req.params.id +"|NA|210.00|NA|NA|NA|INR|NA|R|"+ process.env.BILLDESK_SECURITY_ID +"|NA|NA|F|NA|NA|NA|NA|NA|NA|NA|http://localhost:3000/payment/response";
+    let msg = process.env.BILLDESK_MERCHANT_ID+ "|ARP10234"+ req.params.id +"|NA|210.00|NA|NA|NA|INR|NA|R|"+ process.env.BILLDESK_SECURITY_ID +"|NA|NA|F|NA|NA|NA|NA|NA|NA|NA|http://localhost:3000/payment/response";
 
-    let checksum = crypto.createHmac('sha256', process.env.BILLDESK_SECURITY_ID).update(msg).digest('hex');
+    let checksum = crypto.createHmac('sha256', process.env.BILLDESK_CHECKSUM_KEY).update(msg).digest('hex').toUpperCase();
 
     res.json({
         status: "success",
